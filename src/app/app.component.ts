63dab78ca5183fec5adfb2390be9d3db70225fff
Jason <floresj4@gmail.com>
2018-07-02 17:16:45 -0400

    adding bootstrap and starting simple list with drag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  listData: string[];
+
+  onDragStart($event) {
+    console.log($event);
+  }
+
+  ngOnInit() {
+    this.listData = [
+      'Hot water heater',
+      'Digital locks',
+      'Curtain hooks'
+    ];
+  }
 }
